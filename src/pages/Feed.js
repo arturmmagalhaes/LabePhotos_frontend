1ba2cb29fcad63061b0afc65267ec3e6596dee5c
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-09-14 23:03:07 -0300

    logic Home, SignIn and SignUp

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as Div from '../components/Divs';
 import Footer from '../components/Footer';
+import api from '../services/api';
+
 export default function Feed() {
+    const [token] = useState(JSON.parse(localStorage.getItem('token')));
+    
+    useEffect(() => {
+        //api.get()
+    },[]);
+    
     return(
         <Div.SecundaryContainer>
             <Div.Header>LABEPHOTOS</Div.Header>
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,18 +1,59 @@
 import React from 'react';
+import api from '../services/api';
+import useForm from '../customHooks/useForm';
 import * as Div from '../components/Divs';
 import * as Input from '../components/Inputs';
 import * as Buttons from '../components/Buttons';
+import { useHistory } from 'react-router-dom';
 
 export default function SignUp() {
+    const history = useHistory();
+    const { form, handleForm } = useForm({
+      email: '',
+      password: ''
+    });
+
+    const handleInput = event => {
+      const {name, value} = event.target;
+      handleForm(name, value);
+    }
+
+    const onSubmitFunction = (event) => {
+      event.preventDefault();
+
+      api.post('signin', form)
+        .then(response => {
+          history.push('feed');
+          localStorage.setItem("token", JSON.stringify(response.data.token));
+          console.log(response);
+        })
+        .catch(error => {
+          alert("Algo deu errado!");
+        })
+    }
+
     return (
         <Div.SecundaryContainer>
           <Div.Header>SIGN IN</Div.Header>
           <Div.DivContent>
-            <Div.DivInputs>
-              <Input.InputDefault placeholder="email"/>
-              <Input.InputDefault placeholder="password"/>
-              <Buttons.ConfirmButton>submit</Buttons.ConfirmButton>
-            </Div.DivInputs>
+            <form onSubmit={onSubmitFunction}>
+              <Div.DivInputs>
+                <Input.InputDefault 
+                  name="email"
+                  placeholder="email"
+                  onChange={handleInput}
+                  value={form.email}
+                  required />
+                <Input.InputDefault 
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  onChange={handleInput}
+                  value={form.password}
+                  required />
+               <Buttons.ConfirmButton>submit</Buttons.ConfirmButton>
+              </Div.DivInputs>
+            </form>
           </Div.DivContent>
         </Div.SecundaryContainer>
     )
