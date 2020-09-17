e956e94f2fb9c3d9df3c0f548c9f9d098a370505
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2020-09-17 10:43:29 -0300

    Adjusts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import * as Div from './Divs';
+import { makeStyles } from '@material-ui/core/styles';
 import HomeIcon from '@material-ui/icons/Home';
 import PersonIcon from '@material-ui/icons/Person';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
+const useStyles = makeStyles({
+  root: {
+    cursor: 'pointer',
+  },
+});
+
 export default function Footer() {
+  
     const history = useHistory();
+
     const logOut = () => {
       localStorage.clear();
       history.replace('/signin');
@@ -21,7 +30,7 @@ export default function Footer() {
           <Link to="/profile"><PersonIcon /></Link>
         </Div.Icon>
         <Div.Icon>
-          <ExitToAppIcon onClick={logOut}/>
+          <ExitToAppIcon className={useStyles().root} onClick={logOut}/>
         </Div.Icon>
     </Div.Footer>
     )
